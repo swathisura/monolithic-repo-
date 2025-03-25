fc916e602a18803b2cd059f7b9e5a6cfc977adf9
shaikmustafa77 <awsanddevops37@gmail.com>
2025-03-25 19:40:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0454e52560c7f5c55"
+    image_id           = "ami-04aa00acb1165b32a"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "lastone"
+    key_name = "new-kp-test"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-024286466e258f339", "subnet-09558309acf4661a8"]
+     subnets = ["subnet-0b38d40a98d1527c1", "subnet-0a75ec2e6a09a1ae6"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
