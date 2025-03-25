0d6228c1a45b8487351b149b61921572b88b4051
shaikmustafa77 <awsanddevops37@gmail.com>
2025-03-25 19:41:26 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafashaik77.devops.project.bucket"
+  bucket = "shaikmustafa.flm.devops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -22,10 +22,4 @@ versioning_configuration {
 status = "Enabled"
 }
 }
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "mustafashaik77.devops.project.bucket"
-key = "prod/terraform.tfstate"
-}
-}
+
