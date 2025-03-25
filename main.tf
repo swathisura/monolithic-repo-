b5a48a2aed8575e5b068fdfe07212e9cb8bcb74b
shaikmustafa77 <awsanddevops37@gmail.com>
2025-03-25 20:19:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_launch_template" "web_server_as" {
      security_groups = [aws_security_group.web_server.id]
      subnets = ["subnet-0b38d40a98d1527c1", "subnet-0a75ec2e6a09a1ae6"]
      listener {
-      instance_port     = 8000
+      instance_port     = 80
       instance_protocol = "http"
       lb_port           = 80
       lb_protocol       = "http"
