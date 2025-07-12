81df13f44f7dbf130f1a1b68b786aef8a0f89821
shaikmustafa77 <awsanddevops37@gmail.com>
2025-07-12 21:27:51 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "shaikmustafa.flm.devops.project.bucket"
+  bucket = "shaikmustafa77.flm.devsecops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
