933890bcbbce65fa3582390858e0ba98acfacf3a
swathisura <swathisura65@gmail.com>
2025-07-14 16:34:30 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "shaikmustafa77.flm.devsecops.project.bucket"
+  bucket = "swathi30.flm.devsecops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
